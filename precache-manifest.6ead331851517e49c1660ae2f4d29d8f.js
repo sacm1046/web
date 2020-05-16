e126db513066202367c65ffad5bf7a9568ae6ec4
Sebastian Contreras <sacm1046@gmail.com>
2020-05-16 11:26:01 -0400

    Updates

---
+++ precache-manifest.6ead331851517e49c1660ae2f4d29d8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e290acbf00c83306c2189dad0b3dea2d",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "cc0d4a79d48570120e29",
+    "url": "/web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "75031daef2459c216b20",
+    "url": "/web/static/js/2.cf403278.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web/static/js/2.cf403278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc0d4a79d48570120e29",
+    "url": "/web/static/js/main.a8022731.chunk.js"
+  },
+  {
+    "revision": "6a80518551623f48d6dd",
+    "url": "/web/static/js/runtime-main.89b87c4e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web/static/media/logo.5d5d9eef.svg"
+  }
+]);
